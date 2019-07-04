301ae5bddad4aa3d78d400f40651f3c08f962282
Tomonari Takai <takai01010100@gmail.com>
2019-07-04 10:18:07 +0900

    use googlefont:noto sans jp
    refs: https://fonts.google.com/specimen/Noto+Sans+JP?selection.family=Noto+Sans+JP:400,700,900

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,15 +4,8 @@
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+  font-family: 'Noto Sans JP', sans-serif;
+  font-size: 10px;
 }
 
 *,
